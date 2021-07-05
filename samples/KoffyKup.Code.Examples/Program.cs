c1a511305c8d617d7c6a0ab00f3ded99572d4874
Raghavendra NV <nv.ragu@gmail.com>
2021-07-05 21:54:27 +0530

    Remove older classes. Add tests

---
+++ samples/KoffyKup.Code.Examples/Program.cs
@@ -0,0 +1,28 @@
+﻿using KoffyKup.Code;
+using System;
+namespace KoffyKup.BeginEnd.Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CodeExternsionsExample();
+            Console.ReadLine();
+        }
+
+        private static void CodeExternsionsExample()
+        {
+            CodeExtensions.Code(
+            () =>
+            {
+                var i = 100;
+                i++;
+                throw new Exception("General exception");
+            }
+            ).MeasureTime((x) => Console.WriteLine(x.CpuTime))
+            .HandleException((ex) => Console.WriteLine(ex.Message))
+            .PreAndPost(() => Console.WriteLine("PreAction"), () => Console.WriteLine("PostAction"))
+            ();
+        }
+    }
+}
---
+++ tests/KoffyKup.CodeExtensions.Test/CodeExtensionTest.cs
@@ -0,0 +1,59 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KoffyKup.Code;
+
+namespace KoffyKup.CodeExtensions.Test
+{
+    [TestClass]
+    public class CodeExtensionTest
+    {
+        [TestMethod]
+        public void Test_WhenPrePostActionsProvidedShouldSucceed()
+        {
+            var flag = "";
+            Code.CodeExtensions.Code(() =>
+           {
+               Assert.AreEqual("PreExecuted", flag);
+           })
+             .PreAndPost(() => flag = "PreExecuted", () => flag = "PostExecuted")
+             ();
+            Assert.AreEqual("PostExecuted", flag);
+        }
+
+        [TestMethod]
+        public void Test_WhenPrePostActionsProvidedAndWithExceptionShouldExecuteActions()
+        {
+            var preFlag = ""; var postFlag = "";
+            try
+            {
+                Code.CodeExtensions.Code(() =>
+                {
+                    throw new System.Exception("error");
+                })
+                 .PreAndPost(() => preFlag = "PreExecuted", () => postFlag = "PostExecuted")
+                 ();
+            }
+            catch {; }
+            Assert.AreEqual("PreExecuted", preFlag);
+            Assert.AreEqual("PostExecuted", postFlag);
+        }
+        [TestMethod]
+        public void Test_WhenTimeMetricRequestedShould()
+        {
+            Code.CodeExtensions.Code(() =>
+            {
+                for (int i = 0; i < 10000000; i++)
+                {
+                    var j = i + i;
+                }
+                System.Threading.Thread.Sleep(1000);
+            }).MeasureTime(metric =>
+            {
+                Assert.IsTrue(metric.CpuTime > 0);
+                Assert.IsTrue(metric.WallClockTime > 0);
+                Assert.IsTrue(metric.StartAt > 0);
+                Assert.IsTrue(metric.EndAtt > 0);
+            })
+             ();
+        }
+    }
+}
